ba24b4bcfb448fb9dee4632c6a6c0b536f3bdbdd
Arjun-Zemoso <arjun.singh@zemosolabs.com>
2023-05-19 09:12:06 +0530

    Removed unnecessary comments.

--- src/main/java/com/example/dtodemo/service/UserService.java
+++ src/main/java/com/example/dtodemo/service/UserService.java
@@ -20,19 +20,10 @@ public class UserService {
     public List<UserLocationDTO> getAllUsersLocations(){
         return  userRepository.findAll().stream().map(this::convertEntityToDTO).collect(Collectors.toList());
     }
-//    private  UserLocationDTO convertEntityToDTO(User user){
-//        UserLocationDTO userLocationDTO = new UserLocationDTO();
-//        userLocationDTO.setUserId(user.getId());
-//        userLocationDTO.setEmail(user.getEmail());
-//        userLocationDTO.setPlace(user.getLocation().getPlace());
-//        userLocationDTO.setLongitude(user.getLocation().getLongitude());
-//        userLocationDTO.setLatitude(user.getLocation().getLatitude());
-//        return  userLocationDTO;
-//    }
+
     private  UserLocationDTO convertEntityToDTO(User user){
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-        UserLocationDTO userLocationDTO = new UserLocationDTO();
-        userLocationDTO=modelMapper.map(user,UserLocationDTO.class);
-        return  userLocationDTO;
+        return modelMapper.map(user,UserLocationDTO.class);
+
     }
 }
